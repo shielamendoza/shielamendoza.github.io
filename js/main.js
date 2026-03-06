e64c9de7363f07aefb2d61243603995265e15683
mobiledev-viptutors <mobiledev@viptutors.co>
2026-03-06 19:04:48 +0800

    chore: update portfolio

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@
 
 
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar.sticky-top').addClass('shadow-sm navbar-scrolled');
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $('.navbar.sticky-top').removeClass('shadow-sm navbar-scrolled');
         }
     });
     
